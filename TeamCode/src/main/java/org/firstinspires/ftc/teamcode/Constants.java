b504b30da1b37ae385b61c607c7596360ee42c3b
Ddundee <dhanush.actual1@gmail.com>
2022-10-06 19:58:09 -0500

    Make Constants & Machine States (for FSM)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Write out Documentation Code
+ */
+
+public class Constants {
+    public static class DriveTrainConstants {
+        final public static double MAX_SPEED = 1; // TODO: Change appropriately when driving is tested
+        final public static String LF_MAP_NAME = "lf";
+        final public static String RF_MAP_NAME = "rf";
+        final public static String LB_MAP_NAME = "lb";
+        final public static String RB_MAP_NAME = "rb";
+    }
+}
