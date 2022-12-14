c7a187de1108ef88426aba82dced8fdbd34cf1af
Saucy11077 <sahaskumaraguru@gmail.com>
2022-12-14 15:52:26 -0600

    i forgot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantsAndStuff/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantsAndStuff/Constants.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.ConstantsAndStuff;
 
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.getMotorVelocityF;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.rpmToVelocity;
+
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
 /**
  * Write out Documentation Code
  */
@@ -37,5 +42,34 @@ public class Constants {
 
     public boolean deployed = false;
     public boolean intake = true;
+
+
+    public static final boolean RUN_USING_ENCODER = true;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
+            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    // Dt constants
+    // 19.2:1 gear ratio
+    public static final double TICKS_PER_REV = 537.7;
+    public static final double MAX_RPM = 312;
+
+    // Wheel
+    public static double WHEEL_R_MM = 48;
+    public static final double MM_IN = 25.4;
+
+    public static double WHEEL_RADIUS = WHEEL_R_MM / MM_IN;
+    public static double GEAR_RATIO = 1;
+    public static double TRACK_WIDTH = 13.57;
+
+    public static double kV = 12.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0;
+    public static double kStatic = 0;
+
+    //public static double MAX_VEL = (MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * (Math.PI * 2);
+    public static double MAX_VEL = 30;
+    public static double MAX_ACCEL = 30;
+    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_ACCEL = Math.toRadians(180);
+
+
 
 }
