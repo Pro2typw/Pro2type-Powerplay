6e5cbb6bb6b459f365c231cb113a95b5aa5c8e6e
jayant <jbhaskaruni@gmail.com>
2022-12-30 15:02:10 -0600

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantsAndStuff/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantsAndStuff/Constants.java
@@ -66,7 +66,4 @@ public class Constants {
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
-
-
-
 }
